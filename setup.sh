13d21a23ca2f58868fb7b9403895d8f7fde0a138
yudppp <yu.d.ppp@gmail.com>
2016-06-23 11:59:43 +0900

    Add git alias

--- setup.sh
+++ setup.sh
@@ -13,3 +13,9 @@ cd ~/.vim/bundle/
 git clone https://github.com/Shougo/neobundle.vim.git neobundle.vim
 
 ln -sf dotfiles/.vimrc .vimrc
+
+git config --global alias.br branch
+git config --global alias.ba branch -a
+git config --global alias.co checkout
+git config --global alias.cp cherry-pick
+git config --global alias.st status
